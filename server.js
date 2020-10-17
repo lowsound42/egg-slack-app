901cf8d64010e5342ccfbb8ce20fe42c4d0b8017
omario <o.khandxb@gmail.com>
2020-10-17 13:23:33 -0400

    haha

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 8042
 const http = require('http')
+const axios = require("axios")
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("I'M ALIVE!")
   console.log("something happened")
 })
+
 
 
 app.post("/test", (req, res) => {
@@ -49,6 +56,28 @@ app.post("/test", (req, res) => {
       console.log("Error: " + err.message);
   })
 })
+
+
+app.post("/light", (request, res) => {
+  console.log(request.body.text)
+  var thing = request.body.text
+
+  if (thing == "on")
+  {
+    axios.put('http://192.168.0.106/api/Kh4lvPekMerBQslndYvx0Z2Lwh-ITbOwNBK8yYjP/lights/3/state', {
+      "on": true
+    })
+  }
+
+  if (thing == "off")
+  {
+    axios.put('http://192.168.0.106/api/Kh4lvPekMerBQslndYvx0Z2Lwh-ITbOwNBK8yYjP/lights/3/state', {
+      "on": false
+    })
+  }
+  res.send("You're messing with my lights")
+})
+
 
 
 app.listen(port, () => {
