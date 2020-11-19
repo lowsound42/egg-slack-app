10afa1b8a778414ed9fa73a734473b70192f2a01
omario <o.khandxb@gmail.com>
2020-11-19 16:21:20 -0500

    test

--- server.js
+++ server.js
@@ -12,12 +12,16 @@ app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("I'M ALIVE!")
   console.log("something happened")
 })
-
-
 
 app.post("/test", (req, res) => {
   http.get("http://itsthisforthat.com/api.php?json", (resp) => {
